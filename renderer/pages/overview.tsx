27dc004f3b1973ae81f87da289b8401bfc7d342e
alexaib <alexaib@outlook.es>
2023-05-27 14:52:08 +0200

    Implemented column query in DB table view

--- renderer/pages/overview.tsx
+++ renderer/pages/overview.tsx
@@ -12,48 +12,16 @@ import * as React from 'react';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
 import { ipcRenderer } from 'electron';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  // { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  // { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  // { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  // { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  // { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  // { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  // { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  // { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  // { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 const Root = styled('div')(({ theme }) => {
@@ -82,7 +50,7 @@ function Overview() {
     };
     updateTabs();
   }, []);
-
+  
   // Requests the fields of the selected DB table. Updates every time tabValue changes.
   React.useEffect(() => {
     const updateTabFields = () => {
@@ -139,58 +107,67 @@ function Overview() {
   function DataDisplay(props: any) {
     const { tabId } = props;
 
-  function AdditionDialog() {
-    const closeDialog = () => setAddDialogOpen(false);
-    return (
-      <Dialog open={addDialogOpen} onClose={closeDialog}>
-        <DialogTitle>Add new entry</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {/*
-              TODO Maybe there should be a table containing a description for each DB table.
-            */}
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="field"
-            label="Field"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={closeDialog}>
-            Cancel
-          </Button>
-          <Button color="primary" onClick={undefined}>
-            Confirm
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+    const columns: GridColDef[] = tabFields.map((dbField: string) => {
+      return {
+        field: dbField,
+        headerName: dbField.toUpperCase(),
+        width: 150,
+        editable: true,
+      };
+    });
 
-  function DeletionDialog() {
-    const closeDialog = () => setDelDialogOpen(false);
-    return (
-      <Dialog open={delDialogOpen} onClose={closeDialog}>
-        <DialogTitle>Delete entries</DialogTitle>
-        <DialogContent>
-          Do you really want to delete the selected items?
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={closeDialog}>
-            Cancel
-          </Button>
-          <Button color="primary" onClick={undefined}>
-            Confirm
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+    function AdditionDialog() {
+      const closeDialog = () => setAddDialogOpen(false);
+      return (
+        <Dialog open={addDialogOpen} onClose={closeDialog}>
+          <DialogTitle>Add new entry</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {/*
+              TODO Maybe there should be a table containing a description for each DB table.
+              */}
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="field"
+              label="Field"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={closeDialog}>
+              Cancel
+            </Button>
+            <Button color="primary" onClick={undefined}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Dialog>
+      );
+    }
+  
+    function DeletionDialog() {
+      const closeDialog = () => setDelDialogOpen(false);
+      return (
+        <Dialog open={delDialogOpen} onClose={closeDialog}>
+          <DialogTitle>Delete entries</DialogTitle>
+          <DialogContent>
+            Do you really want to delete the selected items?
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={closeDialog}>
+              Cancel
+            </Button>
+            <Button color="primary" onClick={undefined}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Dialog>
+      );
+    }
 
     return (
       <div
